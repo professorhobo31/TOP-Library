a882e745607d8ee1e3a429c7a8f9e771ea7298b3
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-05-21 11:45:29 -0300

    Initial grid setup

---
+++ scripts.js
@@ -0,0 +1,4 @@
+//The main purpose of our scripts will be threefold. Adding a button on the side of each moused-over row to
+//have the ability of deleting books from the list. Having a working button than, when pressed, brings up a
+//form to enter the info of a new book. Handling the output of the form to, instead of sending the data to
+//a non-existing server, add a new row w/the info of the book.
