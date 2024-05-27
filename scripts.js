e5c5469465806056ae25078d3e7dae92156d33e3
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-05-27 17:22:08 -0300

    Re-organize code, delete unused parts, add basic button functionality

--- scripts.js
+++ scripts.js
@@ -3,7 +3,13 @@
 //form to enter the info of a new book. Handling the output of the form to, instead of sending the data to
 //a non-existing server, add a new row w/the info of the book.
 
-/**This function creates book objects which have a function to log some info about each book into the console
+/**
+ * @type {array} - Saves all books introduced by the user.
+ */
+const myLibrary = [];
+
+/**This function creates book objects, objects which have a function to log some info about each book 
+ * into the console.
  * @param {string} title - Title of the book
  * @param {string} author - Name of the author(s)
  * @param {string} pages - Number of pages
@@ -15,18 +21,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;  //this last one should be a true or false
-
-    /**This function logs each book's info to the console, upon call. 
-     * @returns {string}
-     */
-    this.bookInfo = function () {
-        if (this.read === false) {
-            return console.log(`${this.title} by ${this.author}, ${this.pages} pages, not read yet.`);
-        }
-        else {
-            return console.log(`${this.title} by ${this.author}, ${this.pages} pages, has been read already.`);
-        }
-    }
+    myLibrary.push(this); //stores the book to our array
 
     /**This function adds all the book info to a new row on our list, upon call.
      * @returns {div}
@@ -57,29 +52,29 @@ function Book(title, author, pages, read) {
         }
         container.appendChild(content5);
     }
+}
 
-        myLibrary.push(this);
+function newBookPopup() {
+    alert('Popup works')
 }
 
 const container = document.getElementById("dataContainer");
+const newBookButton = document.getElementById("addBookBtn");
+newBookButton.addEventListener('click', newBookPopup);
 
-/**
- * @type {array} - Saves all books introduced by the user.
- */
-const myLibrary = [];
-
-function addBookToLibrary() {
-    // do stuff here
-  }
-
-
+//in this strip I should add  the code that creates new book entries
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
 const book2 = new Book('Peter Capusotto: El Libro', 'Diego Capusotto & Pedro Saborido', '237', true);
 const book3 = new Book('World War Z', 'Max Brooks', '457', true);
-const book4 = new Book('El Eternauta', 'Hector G Oesterheld & Francisco Solano Lopez', "351", false)
-book1.bookInfo();
-book2.bookInfo();
+const book4 = new Book('El Eternauta', 'Hector G Oesterheld & Francisco Solano Lopez', "351", false);
 book2.newRow();
 book1.newRow();
 book4.newRow();
 book3.newRow();
+//it should finish here
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
+myLibrary.forEach((Book) => console.log(Book));
