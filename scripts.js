3117c94cac3e77e0c27e03375a3b3bb3a97377e1
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-05-28 16:02:37 -0300

    Reorganize code, first attempt at add button functionality

--- scripts.js
+++ scripts.js
@@ -8,8 +8,7 @@
  */
 const myLibrary = [];
 
-/**This function creates book objects, objects which have a function to log some info about each book 
- * into the console.
+/**This function creates book objects.
  * @param {string} title - Title of the book
  * @param {string} author - Name of the author(s)
  * @param {string} pages - Number of pages
@@ -22,61 +21,66 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;  //this last one should be a true or false
     myLibrary.push(this); //stores the book to our array
+}
 
-    /**This function adds all the book info to a new row on our list, upon call.
+/**
+ * This function adds a new book to our array upon press of a button, after filling up the form.
+ */
+function addToLibrary() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    let book = new Book(title, author, pages, read);
+    console.log(book);
+}
+
+/**This function adds all the book info to a new row on our list, upon call. IN REWORK
      * @returns {div}
      */
-    this.newRow = function () {
-        const content1 = document.createElement('div');
-        content1.textContent = `${this.title}`;
-        container.appendChild(content1);
-        // alert('newrow enabled');
-        const content2 = document.createElement('div');
-        content2.textContent = `${this.author}`;
-        container.appendChild(content2);
+this.newRow = function (book) {
+    const content1 = document.createElement('div');
+    content1.textContent = `${book.title}`;
+    container.appendChild(content1);
+    // alert('newrow enabled');
+    const content2 = document.createElement('div');
+    content2.textContent = `${book.author}`;
+    container.appendChild(content2);
 
-        const content3 = document.createElement('div');
-        content3.textContent = `${this.title}`;
-        container.appendChild(content3);
+    const content3 = document.createElement('div');
+    content3.textContent = `${book.title}`;
+    container.appendChild(content3);
 
-        const content4 = document.createElement('div');
-        content4.textContent = `${this.pages}`;
-        container.appendChild(content4);
+    const content4 = document.createElement('div');
+    content4.textContent = `${book.pages}`;
+    container.appendChild(content4);
 
-        const content5 = document.createElement('div');
-        if (this.read === true) {
-            content5.textContent = 'Yes';
-        }
-        else {
-            content5.textContent = 'No';
-        }
-        container.appendChild(content5);
+    const content5 = document.createElement('div');
+    if (book.read === true) {
+        content5.textContent = 'Yes';
     }
+    else {
+        content5.textContent = 'No';
+    }
+    container.appendChild(content5);
 }
 
 const dialog = document.getElementById('modal')
-function newBookPopup() {
+function modalPopup() {
     //alert('Popup works')
     dialog.showModal();
 }
 
 const container = document.getElementById('dataContainer');
 const newBookButton = document.getElementById('addBookBtn');
-newBookButton.addEventListener('click', newBookPopup);
+newBookButton.addEventListener('click', modalPopup);
+
+const submitButton = document.getElementById('submitButton');
 
 //in this strip I should add  the code that creates new book entries
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
 const book2 = new Book('Peter Capusotto: El Libro', 'Diego Capusotto & Pedro Saborido', '237', true);
 const book3 = new Book('World War Z', 'Max Brooks', '457', true);
 const book4 = new Book('El Eternauta', 'Hector G Oesterheld & Francisco Solano Lopez', '351', false);
-book2.newRow();
-book1.newRow();
-book4.newRow();
-book3.newRow();
+console.log(myLibrary)
 //it should finish here
-
-function addBookToLibrary() {
-    // do stuff here
-}
-
-myLibrary.forEach((Book) => console.log(Book));
